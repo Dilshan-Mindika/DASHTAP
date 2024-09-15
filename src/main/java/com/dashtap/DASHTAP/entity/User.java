7a27a7e4dd6351b667a2a1fb3e58322312c465b3
Dilshan Mindika <maxdilshan666@gmail.com>
2024-09-15 14:23:28 +0530

    Initial commit

---
+++ src/main/java/com/dashtap/DASHTAP/entity/User.java
@@ -0,0 +1,12 @@
+package com.dashtap.DASHTAP.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+
+public class User {
+}
