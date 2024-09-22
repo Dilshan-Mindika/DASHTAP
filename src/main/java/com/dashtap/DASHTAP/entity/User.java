00ecbda76c15752aa88c292f158153c9fd21d82a
Dilshan Mindika <maxdilshan666@gmail.com>
2024-09-22 21:06:25 +0530

    updated

--- src/main/java/com/dashtap/DASHTAP/entity/User.java
+++ src/main/java/com/dashtap/DASHTAP/entity/User.java
@@ -3,12 +3,17 @@ package com.dashtap.DASHTAP.entity;
 import com.dashtap.DASHTAP.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name = "users")
-
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,8 +25,35 @@ public class User {
 
     private String password;
 
-    private UserRole role;
+    private UserRole userRole;
 
-    public void setUserRole(UserRole userRole) {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userRole.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
